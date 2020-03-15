2b835795cb9d17831664ec8373edd33fb9b6a838
unknown <C:\Users\\AppData\Roaming\The Bat!>
2020-03-15 10:14:11 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,33 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let arr=[];
+    if(matrix==undefined){
+     return arr;   
+    } else{
+    
+   
+
+    for (let i=0; i<=matrix.length-1;i++){
+        matrix[i].sort( (a, b) => a - b );
+        if(i%2 == 0) {
+            for (let j=0; j<=matrix[i].length-1;j++){
+            
+             arr.push(matrix[i][j]);
+         } 
+        }
+        else {
+            
+             matrix[i].reverse();
+             for (let j=0; j<=matrix[i].length-1;j++){
+         
+         arr.push(matrix[i][j]);
+         }
+        }
+    }
+       
+
+  
+    return arr;
+}
 }
